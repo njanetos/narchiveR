fe264269af91c7fd7b4745f3973bbb85dbec5f81
Jan Tilly <jantilly@gmail.com>
2015-06-10 12:51:35 -0400

    added plot by bin

--- demo/plot-by-week.R
+++ demo/plot-by-week.R
@@ -14,16 +14,23 @@ mdma = get.query("SELECT * FROM Listing L
 mdma$price = as.numeric(mdma$price)
 mdma$price.normalized = mdma$price / (mdma$mult * mdma$amount)
 mdma$timestamp = mdma$date
-rm(mdma$date)
 mdma$day = as.Date(as.POSIXct(mdma$timestamp, origin = "1970-01-01"))
 mdma$week = format(mdma$day, "%Y-%U")
 # sort by date
 mdma = mdma[order(mdma$timestamp),]
+# group the data
+k = 5
+n = length(mdma$price)
+mdma$bin = rep(1:ceiling(n/k), each = k)[1:n]
 
 # create data table
 mdma.dt = data.table(mdma)
 prices.by.day = data.frame(mdma.dt[,list(median = median(price.normalized)), by = day])
 prices.by.week = data.frame(mdma.dt[,list(median = median(price.normalized)), by = week])
+prices.by.bin = data.frame(mdma.dt[,list(median = median(price.normalized)), by = bin])
+
+# plot prices by bin
+plot(prices.by.bin$median, type = "l",  xaxt = 'n', xlab = "Bin", ylab = "Price in Dollars per 100mg")
 
 # plot weekly prices
 plot(prices.by.week$median, type = "l",  xaxt = 'n', xlab = "Week", ylab = "Price in Dollars per 100mg")
