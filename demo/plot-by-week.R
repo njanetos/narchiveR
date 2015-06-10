956092e554fb3773a16d4cc84a608d5347903ded
Jan Tilly <jantilly@gmail.com>
2015-06-10 13:07:49 -0400

    k-median bins can now be plotted as a function of date

--- demo/plot-by-week.R
+++ demo/plot-by-week.R
@@ -11,29 +11,35 @@ mdma = get.query("SELECT * FROM Listing L
                             WHERE category = 2361707
                             AND denomination = 'USD'
                             AND units = 'mg'")
-mdma$price = as.numeric(mdma$price)
+mdma$price = 100 * as.numeric(mdma$price)
 mdma$price.normalized = mdma$price / (mdma$mult * mdma$amount)
 mdma$timestamp = mdma$date
 mdma$day = as.Date(as.POSIXct(mdma$timestamp, origin = "1970-01-01"))
 mdma$week = format(mdma$day, "%Y-%U")
 # sort by date
 mdma = mdma[order(mdma$timestamp),]
-# group the data
-k = 5
+
+# group the data with binsize k
+k = 1000
 n = length(mdma$price)
 mdma$bin = rep(1:ceiling(n/k), each = k)[1:n]
 
 # create data table
 mdma.dt = data.table(mdma)
-prices.by.day = data.frame(mdma.dt[,list(median = median(price.normalized)), by = day])
-prices.by.week = data.frame(mdma.dt[,list(median = median(price.normalized)), by = week])
-prices.by.bin = data.frame(mdma.dt[,list(median = median(price.normalized)), by = bin])
+
+# create prices by week
+prices.by.week = data.frame(c(mdma.dt[,list(median = median(price.normalized)), by = week],
+                              mdma.dt[,list(week.day = min(day)), by = week]))
+
+# create prices by bin
+prices.by.bin = data.frame(c(mdma.dt[,list(median = median(price.normalized)), by = bin],
+                             mdma.dt[,list(bin.day = min(day)), by = bin]))
 
 # plot prices by bin
-plot(prices.by.bin$median, type = "l",  xaxt = 'n', xlab = "Bin", ylab = "Price in Dollars per 100mg")
+plot(as.Date(prices.by.bin$bin.day), prices.by.bin$median, type = "l",  xlab = "Date",
+     ylab = "Price in Dollars per 100mg", ylim = c(1.5,5))
 
 # plot weekly prices
-plot(prices.by.week$median, type = "l",  xaxt = 'n', xlab = "Week", ylab = "Price in Dollars per 100mg")
-axis(side = 1, at = seq(from = 1, to = length(prices.by.week$week), length.out = 10),
-             labels = prices.by.week$week[seq(from = 1, to = length(prices.by.week$week), length.out = 10)])
+plot(as.Date(prices.by.week$week.day), prices.by.week$median, type = "l",  xlab = "Date",
+     ylab = "Price in Dollars per 100mg", ylim = c(1.5,5))
 
